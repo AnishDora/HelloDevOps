df482e032c81e1b91d518d6a0fa31a256e4ea495
AnishDora <65508519+AnishDora@users.noreply.github.com>
2026-04-22 15:12:26 -0700

    initial full stack devops

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  const loadMessages = () => {
+    fetch("/api/messages")
+      .then((res) => res.json())
+      .then((data) => setMessage(JSON.stringify(data)))
+      .catch(() => setMessage("API failed"));
+  };
+
+  const addMessage = () => {
+    fetch("/api/messages", {
+      method: "POST",
+    }).then(() => loadMessages());
+  };
+
+  useEffect(() => {
+    loadMessages();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <p>{message}</p>
+      <button onClick={addMessage}>Add Message</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+// DB connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("DB connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+// Routes
+app.get("/api/messages", (req, res) => {
+  db.query("SELECT * FROM messages", (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "DB query failed" });
+    }
+    res.json(results);
+  });
+});
+
+app.post("/api/messages", (req, res) => {
+  db.query("INSERT INTO messages (text) VALUES (?)",["Hello from MySQL"],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Insert failed" });
+      }
+      res.json({ message: "Inserted", id: result.insertId });
+    }
+  );
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
